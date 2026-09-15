114c4b00436618f8c27f7f3629dd257a91241f7d
Frog-with-Code <frogwithcode@gmail.com>
2026-09-15 13:28:49 +0300

    feat(application): add exception mapping, add new methods

--- src/book_recommendation_bot/application/services.py
+++ src/book_recommendation_bot/application/services.py
@@ -1,8 +1,17 @@
-from book_recommendation_bot.domain.exceptions import NotFoundError
+from book_recommendation_bot.application.exceptions import (
+    BookNotFoundError,
+    RecommendationsExhaustedError,
+    UserAlreadyExistsError,
+    UserNotFoundError,
+)
 from book_recommendation_bot.domain.entities import Book, BookScore, User
 from book_recommendation_bot.domain.repositories import BookRepository, UserRepository
 from book_recommendation_bot.domain.utils import calculate_ema_taste_vector
 from book_recommendation_bot.infrastructure.embeddings import EmbeddingService
+from book_recommendation_bot.infrastructure.exceptions import (
+    RecordNotFoundError,
+    UniqueConstraintViolationError,
+)
 
 
 class RecommendationService:
@@ -23,36 +32,83 @@ class RecommendationService:
         book_description: str,
         liked_genres: list[str],
     ) -> None:
-        user = User(id=user_id, username=username)
-
         taste_vec = self.embedding_service.embed_query(book_description)
 
-        await self.user_repo.create(
-            user=user, taste_vector=taste_vec, liked_genres=liked_genres
+        user = User(
+            id=user_id,
+            username=username,
+            taste_vec=taste_vec,
+            liked_genres=liked_genres,
         )
 
+        try:
+            await self.user_repo.create(user=user)
+        except UniqueConstraintViolationError as e:
+            raise UserAlreadyExistsError(str(e)) from e
+
     async def rate_book(self, user_id: int, book_id: int, score: int) -> None:
+        try:
+            book_embedding = await self.book_repo.get_embedding_by_id(book_id)
+        except RecordNotFoundError as e:
+            raise BookNotFoundError(str(e)) from e
+
+        user = await self.user_repo.get_by_id(user_id)
+        if user is None:
+            raise UserNotFoundError(f"User '{user_id}' not found")
+
         await self.user_repo.rate_book(user_id, book_id, score)
 
-        book_embedding = await self.book_repo.get_embedding_by_id(book_id)
-        current_taste = await self.user_repo.get_taste_vector(user_id)
-
-        new_taste = calculate_ema_taste_vector(current_taste, book_embedding, score)
-
+        new_taste = calculate_ema_taste_vector(user.taste_vec, book_embedding, score)
         if new_taste:
-            await self.user_repo.set_taste_vector(user_id, new_taste)
+            user.taste_vec = new_taste
+            await self.user_repo.update_profile(user)
 
     async def get_next_book(self, user_id: int) -> tuple[Book, BookScore]:
-        taste_vec = await self.user_repo.get_taste_vector(user_id)
-        books = await self.book_repo.get_recommendations_by_taste(
-            user_id=user_id, taste_vector=taste_vec, limit=1
-        ) or await self.book_repo.get_fallback_recommendations(user_id=user_id, limit=1)
+        user = await self.user_repo.get_by_id(user_id)
+        if user is None:
+            raise UserNotFoundError(f"User '{user_id}' not found")
+
+        if user.taste_vec is None:
+            books = await self.book_repo.get_fallback_recommendations(
+                user_id=user_id, limit=1
+            )
+        else:
+            books = await self.book_repo.get_recommendations_by_taste(
+                user_id=user_id, taste_vector=user.taste_vec, limit=1
+            ) or await self.book_repo.get_fallback_recommendations(
+                user_id=user_id, limit=1
+            )
 
         if not books:
-            raise NotFoundError("No books was found!")
+            raise RecommendationsExhaustedError("No recommendations were found!")
+
+        return books[0]
+
+    async def get_book_by_description(
+        self, user_id: int, description: str
+    ) -> tuple[Book, BookScore]:
+        if not await self.is_user_registered(user_id):
+            raise UserNotFoundError(f"User '{user_id}' not found")
+
+        desc_vec = self.embedding_service.embed_query(description)
+
+        books = await self.book_repo.get_recommendations_by_taste(
+            user_id=user_id, taste_vector=desc_vec
+        )
+
+        if not books or books[0][1].vector_score < 0.5:
+            raise RecommendationsExhaustedError(
+                "No recommendations with such description were found!"
+            )
 
         return books[0]
 
     async def is_user_registered(self, user_id: int) -> bool:
-        user = await self.user_repo.get_by_id(user_id)
+        user = await self.user_repo.get_by_id_lightweight(user_id)
         return user is not None
+
+    async def select_favorite_genres(self, user_id: int, genres: list[str]) -> None:
+        try:
+            await self.user_repo.batch_like_genres(user_id, genres)
+        except RecordNotFoundError as e:
+            raise UserNotFoundError(str(e)) from e
